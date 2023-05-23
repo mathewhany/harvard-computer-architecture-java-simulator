63736877fb7d0c6393c70bd3d3ed892ed4f9b27a
BolesWaheed <boleswaheed477@gmail.com>
2023-05-23 22:17:46 +0300

    add decode stage

--- src/main/java/ca/Main.java
+++ src/main/java/ca/Main.java
@@ -28,5 +28,7 @@ public class Main {
         } catch (CaException e) {
             System.err.println(e.getMessage());
         }
+        System.out.println("111100000000000".length());
     }
+
 }
