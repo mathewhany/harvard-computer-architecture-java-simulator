156e76a568e42a6b0b6df04137f9a680a78ed8ed
BolesWaheed <boleswaheed477@gmail.com>
2023-05-25 14:41:50 +0300

    reading the data from registers in decoding stage

--- src/main/java/ca/Computer.java
+++ src/main/java/ca/Computer.java
@@ -60,36 +60,57 @@ public class Computer {
     }
 
     private DecodeOutput decode(FetchOutput fetchOutput) {
-        short instruction = fetch().instruction;
-        return  new DecodeOutput(instruction,registerFile);
+        short opcode ;
+        short r1 = 0  ;
+        short r2= 0 ;
+        short immediate  =  0 ;
+        boolean isBranch =false  ;
+        short instruction = fetchOutput.instruction;
+        short maskOpCode = (short)0b1111000000000000 ;
+        opcode = (short) (instruction & maskOpCode);
+        opcode =(short) (opcode >> 12) ;
+        short  maskR1Code  = (short) 0b0000111111000000;
+        r1 = (short) (instruction & maskR1Code);
+        r1 = (short)(r1>>6);
+        switch (opcode) {
+            case 4 : isBranch = true ;
+            case 3 :
+            case 8 :
+            case 9 :
+            case 10 :
+            case 11 : immediate =(short)(0b0000000000111111 & instruction);break;
+            default:r2 = (short)(0b0000000000111111 & instruction);break;
+        }
+        byte r1Data = registerFile.getGeneralPurposeRegister(r1);
+        byte r2Data = registerFile.getGeneralPurposeRegister(r2);
+        return new DecodeOutput(opcode,r1Data,r2Data,immediate,isBranch);
+
     }
 
     public static class DecodeOutput {
         public short opcode;
-        public short r1;
-        public short r2;
+        public short r1Data;
+        public short r2Data;
         public short immediate;
         public boolean isBranch ;
-        public DecodeOutput (short instruction, RegisterFile registerFile) {
-            short maskOpCode = (short)0b1111000000000000 ;
-            opcode = (short) (instruction & maskOpCode);
-            opcode =(short) (opcode >> 12) ;
-            short  maskR1Code  = (short) 0b0000111111000000;
-            r1 = (short) (instruction & maskR1Code);
-            r1 = (short)(r1>>6);
-            switch (opcode) {
-                case 4 : isBranch = true ;
-                case 3 :
-                case 8 :
-                case 9 :
-                case 10 :
-                case 11 : immediate =(short)(0b0000000000111111 & instruction);break;
-                default:r2 = (short)(0b0000000000111111 & instruction);break;
-            }
-            short DataReg1 = registerFile.getGeneralPurposeRegister(r1);
-            short DataReg2 = registerFile.getGeneralPurposeRegister(r2);
+        public DecodeOutput
+                (short opcode
+                , short r1Data
+                , short r2Data
+                , short immediate
+                , boolean isBranch
+                )
+        {
+            this.opcode = opcode ;
+            this.r1Data  = r1Data;
+            this.r2Data = r2Data;
+            this.immediate = immediate;
+            this.isBranch = isBranch;
 
         }
+
+
+
     }
 
     private void execute(DecodeOutput decodeOutput) {
